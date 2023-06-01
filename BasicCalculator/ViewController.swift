7f997b75aa606fa5fdd1d3d2b3d837a9f13ba30f
kadironer <kadironer46500@gmail.com>
2023-06-01 13:35:55 +0300

    Initial Commit

---
+++ BasicCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicCalculator
+//
+//  Created by Kadir Öner on 1.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
