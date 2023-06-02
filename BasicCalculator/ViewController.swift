45cc283020611319553ed3388370b5b372d3fc94
kadironer <kadironer46500@gmail.com>
2023-06-02 17:20:32 +0300

    Finish app

--- BasicCalculator/ViewController.swift
+++ BasicCalculator/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tfTwo: UITextField!
+    @IBOutlet weak var tfOne: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    var result = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resultLabel.text = String(result)
     }
 
-
+    @IBAction func gather(_ sender: Any) {
+        if let firstNumber = Double(tfOne.text!){
+            if let secondNumber = Double(tfTwo.text!){
+                result = firstNumber + secondNumber
+                resultLabel.text = String(result)
+                tfOne.text = ""
+                tfTwo.text = ""
+            }
+        }
+    }
+    
+    @IBAction func extraction(_ sender: Any) {
+        if let firstNumber = Double(tfOne.text!){
+            if let secondNumber = Double(tfTwo.text!){
+                result = firstNumber - secondNumber
+                resultLabel.text = String(result)
+                tfOne.text = ""
+                tfTwo.text = ""
+            }
+        }
+    }
+    @IBAction func impact(_ sender: Any) {
+        if let firstNumber = Double(tfOne.text!){
+            if let secondNumber = Double(tfTwo.text!){
+                result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+                tfOne.text = ""
+                tfTwo.text = ""
+            }
+        }
+    }
+    @IBAction func divide(_ sender: Any) {
+        if let firstNumber = Double(tfOne.text!){
+            if let secondNumber = Double(tfTwo.text!){
+                result = firstNumber / secondNumber
+                resultLabel.text = String(result)
+                tfOne.text = ""
+                tfTwo.text = ""
+            }
+        }
+    }
 }
 
